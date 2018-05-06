5b0c84c134db9af369a88168511b553d632ea20d
dpf <dpf@dpfs-MacBook-Pro.local>
2018-05-06 00:44:05 -0400

    setup POST product route and product model

--- server.js
+++ server.js
@@ -11,6 +11,20 @@ let Wishlist = require('./model/wishlist');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.post('/product', (req, res) => {
+    let product = new Product();
+    product.title = req.body.title;
+    product.price = req.body.price;
+
+    product.save((err, savedProduct) => {
+        if (err) {
+            res.status(500).send({error: 'Error: could not save product to database.'})
+        }else{
+            res.status(200).send(savedProduct);
+        }
+    })
+});
+
 app.listen(port, () => {
     console.log(`Wishlist API server on port ${port}`);
 });
