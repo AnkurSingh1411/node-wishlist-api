835d1c00180d00a36f1a747a4209bf636a394a6b
dpf <dpf@dpfs-MacBook-Pro.local>
2018-05-06 01:39:00 -0400

    setup/refactored  GET /wishlist, POST /wishlist, and PUT /wishlist/product/add

--- server.js
+++ server.js
@@ -36,6 +36,49 @@ app.get('/product', (req, res) => {
     });
 });
 
+
+app.get('/wishlist', (req, res) => {
+    Wishlist.find({}).populate({path: 'products', model: 'Product'}).exec((err, wishlists) => {
+        if (err) {
+            res.status(500).send({error: 'Error: could not retrieve wishlists'});
+        } else {
+            res.send(wishlists)
+        }
+    })
+});
+
+app.post('/wishlist', (req, res) => {
+    let wishlist = new Wishlist();
+    wishlist.title = req.body.title;
+
+    wishlist.save((err, newWishlist) => {
+        if (err) {
+            res.status(500).send({error: 'Error: could not create Wishlist'});
+        } else {
+            res.send(newWishlist);
+        }
+    })
+});
+
+app.put('/wishlist/product/add', (req, res) => {
+    Product.findOne({_id: req.body.productId}, (err, product) => {
+        if (err) {
+            res.status(500).send({error: 'Error: could not add item to Wishlist'});
+        } else {
+            Wishlist.update({_id: req.body.wishlistId},
+                {$addToSet: {products: product._id}},
+                (err, wishlist) => {
+                    if (err) {
+                        res.status(500).send({error: 'Error: could not add item to Wishlist'});
+                    } else {
+                        res.send(wishlist)
+                    }
+                })
+        }
+    })
+});
+
+
 app.listen(port, () => {
     console.log(`Wishlist API server on port ${port}`);
 });
